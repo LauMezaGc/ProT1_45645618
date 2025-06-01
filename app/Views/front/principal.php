3d19a6748e2f6bd51bd90b4a6cd45d1a9e1f3b85
lau <lautaromezagc@gmail.com>
2025-06-01 17:11:19 -0300

    front finalizado

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,13 +1,13 @@
-	<main>
-		<section>
-			<h1>¡Bienvenido al sitio de TodoElectro!</h1>
+	<main id="pag-principal">
+		<section id="nosotros" class="text-light">
+			<h1 class="fw-bold text-center">¡Bienvenido al sitio de <img class="d-inline-block p-1" style="width: 150px;" src="assets/img/logo1.png">!</h1>
 			<h3>Somos la mejor tienda de electrónica del nordeste argentino.</h3>
-			<p>Aca podes encontrar todo lo que necesitas para tus proyectos independientes.</p>
+			<p class="fw-bold">Aca podes encontrar todo lo que necesitas para tus proyectos independientes.</p>
 		</section>
-		<section>
-			<h1>Lo que busques de electronica...</h1>
+		<section id="productos">
+			<h1 class="text-start">Lo que busques de electronica ...</h1>
 			<!-- carrusel -->
-			<div id="carusel" class="carousel slide object-fit-sm-contain">
+			<div id="carusel" class="carousel slide mx-auto w-75 p-5">
 				<div class="carousel-indicators">
 					<button type="button" data-bs-target="#carusel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
 					<button type="button" data-bs-target="#carusel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -17,47 +17,47 @@
 					<button type="button" data-bs-target="#carusel" data-bs-slide-to="5" aria-label="Slide 6"></button>
 					<button type="button" data-bs-target="#carusel" data-bs-slide-to="6" aria-label="Slide 7"></button>
 				</div>
-				<div class="carousel-inner">
+				<div class="carousel-inner rounded-5 mh-100">
 				    <div class="carousel-item active">
-						<img src="assets/img/carusel_micro.jpg" class="rounded mx-auto d-block" alt="Microcontroladores">
+						<img src="assets/img/carusel_micro.jpg" class="mx-auto d-block mw-100 mh-100" alt="Microcontroladores">
 						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
-							<p>Microcontroladores.</p>
+							<p class="text-center p-2 fs-3">Microcontroladores.</p>
 						</div>
 				    </div>
 				    <div class="carousel-item">
-						<img src="assets/img/carusel_fichas.jpg" class="rounded mx-auto d-block" alt="Fichas">
+						<img src="assets/img/carusel_fichas.jpg" class="mx-auto d-block mw-100 mh-100" alt="Fichas">
 						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
-							<p>Fichas.</p>
+							<p class="text-center p-2 fs-3">Fichas.</p>
 						</div>
 				    </div>
 				    <div class="carousel-item">
-						<img src="assets/img/carusel_resistores.jpg" class="rounded mx-auto d-block" alt="Resistores">
+						<img src="assets/img/carusel_resistores.jpg" class="mx-auto d-block mw-100 mh-100" alt="Resistores">
 						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
-							<p>Resistores.</p>
+							<p class="text-center p-2 fs-3">Resistores.</p>
 						</div>
 				    </div>
 				    <div class="carousel-item">
-						<img src="assets/img/carusel_audio.jpg" class="rounded mx-auto d-block" alt="Audio">
+						<img src="assets/img/carusel_audio.jpg" class="mx-auto d-block mw-100 mh-100" alt="Audio">
 						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
-							<p>Audio.</p>
+							<p class="text-center p-2 fs-3">Audio.</p>
 						</div>
 				    </div>
 				    <div class="carousel-item">
-						<img src="assets/img/carusel_capacitores.jpg" class="rounded mx-auto d-block" alt="Capacitores">
+						<img src="assets/img/carusel_capacitores.jpg" class="mx-auto d-block mw-100 mh-100" alt="Capacitores">
 						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
-							<p>Capacitores.</p>
+							<p class="text-center p-2 fs-3">Capacitores.</p>
 						</div>
 				    </div>
 				    <div class="carousel-item">
-						<img src="assets/img/carusel_diodos.jpg" class="rounded mx-auto d-block" alt="Diodos">
+						<img src="assets/img/carusel_diodos.jpg" class="mx-auto d-block mw-100 mh-100" alt="Diodos">
 						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
-							<p>Diodos.</p>
+							<p class="text-center p-2 fs-3">Diodos.</p>
 						</div>
 				    </div>
 				    <div class="carousel-item">
-						<img src="assets/img/carusel_soldar.jpg" class="rounded mx-auto d-block" alt="Articulos para soldaduras.">
+						<img src="assets/img/carusel_soldar.jpg" class="mx-auto d-block mw-100 mh-100" alt="Articulos para soldaduras.">
 						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
-							<p>Articulos para soldaduras.</p>
+							<p class="text-center p-2 fs-3">Articulos para soldaduras.</p>
 						</div>
 				    </div>
 			  	</div>
@@ -70,21 +70,23 @@
 			    <span class="visually-hidden">Siguiente</span>
 			  </button>
 			</div>
-
+			<h1 class="text-end">... Lo encontras acá!</h1>
 		</section>
-		<section>
+		<section id="contacto">
 			<h1>¡Ponete en contacto con nosotros!</h1>
 			<!-- datos -->
-			<p><b>Nuestro Local:</b> San Luis 1401</p>
-			<p><b>Por Mail:</b> todoelectroarg@gmail.com</p>
-			<p><b>Por telefono:</b> +54 379 5 12-3456</p>
-			<p><b>Nuestras redes:</b></p>
+			<div class="fs-4">
+				<p><b>Nuestro Local:</b> San Luis 1401</p>
+				<p><b>Por Mail:</b> todoelectroarg@gmail.com</p>
+				<p><b>Por telefono:</b> +54 379 5 12-3456</p>	
+				<p><b>Nuestras redes:</b></p>
 				<ul>
 					<li><a href="https://www.facebook.com/">Facebook</a></li>
 					<li><a href="https://www.instagram.com/">Instagram</a></li>
 					<li><a href="https://www.youtube.com/">Youtube</a></li>
 					<li><a href="https://www.linkedin.com/">Linkedin</a></li>
 				</ul>
+			</div>
 		</section>
 
 	</main>
