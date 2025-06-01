94ef49613becd407414986c17a04ceddb0238a28
lau <lautaromezagc@gmail.com>
2025-06-01 08:37:22 -0300

    carusel fix

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,92 +1,90 @@
-	<header>
-		<h1>TODOELECTRO</h1>
-		<nav>
-			<ul>
-				<li><a href="#">Inicio</a></li>
-				<li><a href="#">Productos</a></li>
-				<li><a href="#">Contacto</a></li>
-			</ul>
-		</nav>
-	</header>
 	<main>
 		<section>
 			<h1>¡Bienvenido al sitio de TodoElectro!</h1>
-			<div class="info">
-				<p class="info_subtit">Somos la mejor tienda de electrónica del nordeste argentino.</p>
-				<p>Aca podes encontrar todo lo que necesitas para tus proyectos independientes.</p>
-			</div>
+			<h3>Somos la mejor tienda de electrónica del nordeste argentino.</h3>
+			<p>Aca podes encontrar todo lo que necesitas para tus proyectos independientes.</p>
 		</section>
 		<section>
 			<h1>Lo que busques de electronica...</h1>
 			<!-- carrusel -->
-			<div id="carouselExampleCaptions" class="carousel slide">
-			  <div class="carousel-indicators">
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
-			  </div>
-			  <div class="carousel-inner">
-			    <div class="carousel-item active">
-			      <img src="assets/img/carusel_micro.jpg" class="d-block w-100" alt="Microcontroladores">
-			      <div class="carousel-caption d-none d-md-block">
-			        <p>Microcontroladores.</p>
-			      </div>
-			    </div>
-			    <div class="carousel-item">
-			      <img src="assets/img/carusel_fichas.jpg" class="d-block w-100" alt="Fichas">
-			      <div class="carousel-caption d-none d-md-block">
-			        <p>Fichas.</p>
-			      </div>
-			    </div>
-			    <div class="carousel-item">
-			      <img src="assets/img/carusel_resistores.jpg" class="d-block w-100" alt="Resistores">
-			      <div class="carousel-caption d-none d-md-block">
-			        <p>Resistores.</p>
-			      </div>
-			    </div>
-			    <div class="carousel-item">
-			      <img src="assets/img/carusel_audio.jpg" class="d-block w-100" alt="Audio">
-			      <div class="carousel-caption d-none d-md-block">
-			        <p>Audio.</p>
-			      </div>
-			    </div>
-			    <div class="carousel-item">
-			      <img src="assets/img/carusel_capacitores.jpg" class="d-block w-100" alt="Capacitores">
-			      <div class="carousel-caption d-none d-md-block">
-			        <p>Capacitores.</p>
-			      </div>
-			    </div>
-			    <div class="carousel-item">
-			      <img src="assets/img/carusel_diodos.jpg" class="d-block w-100" alt="Diodos">
-			      <div class="carousel-caption d-none d-md-block">
-			        <p>Diodos.</p>
-			      </div>
-			    </div>
-			    <div class="carousel-item">
-			      <img src="assets/img/carusel_soldar.jpg" class="d-block w-100" alt="Articulos para soldaduras.">
-			      <div class="carousel-caption d-none d-md-block">
-			        <p>Articulos para soldaduras.</p>
-			      </div>
-			    </div>
-			  </div>
-			  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			    <span class="visually-hidden">Previous</span>
+			<div id="carusel" class="carousel slide object-fit-sm-contain">
+				<div class="carousel-indicators">
+					<button type="button" data-bs-target="#carusel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+					<button type="button" data-bs-target="#carusel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					<button type="button" data-bs-target="#carusel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+					<button type="button" data-bs-target="#carusel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+					<button type="button" data-bs-target="#carusel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+					<button type="button" data-bs-target="#carusel" data-bs-slide-to="5" aria-label="Slide 6"></button>
+					<button type="button" data-bs-target="#carusel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+				</div>
+				<div class="carousel-inner">
+				    <div class="carousel-item active">
+						<img src="assets/img/carusel_micro.jpg" class="rounded mx-auto d-block" alt="Microcontroladores">
+						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
+							<p>Microcontroladores.</p>
+						</div>
+				    </div>
+				    <div class="carousel-item">
+						<img src="assets/img/carusel_fichas.jpg" class="rounded mx-auto d-block" alt="Fichas">
+						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
+							<p>Fichas.</p>
+						</div>
+				    </div>
+				    <div class="carousel-item">
+						<img src="assets/img/carusel_resistores.jpg" class="rounded mx-auto d-block" alt="Resistores">
+						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
+							<p>Resistores.</p>
+						</div>
+				    </div>
+				    <div class="carousel-item">
+						<img src="assets/img/carusel_audio.jpg" class="rounded mx-auto d-block" alt="Audio">
+						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
+							<p>Audio.</p>
+						</div>
+				    </div>
+				    <div class="carousel-item">
+						<img src="assets/img/carusel_capacitores.jpg" class="rounded mx-auto d-block" alt="Capacitores">
+						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
+							<p>Capacitores.</p>
+						</div>
+				    </div>
+				    <div class="carousel-item">
+						<img src="assets/img/carusel_diodos.jpg" class="rounded mx-auto d-block" alt="Diodos">
+						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
+							<p>Diodos.</p>
+						</div>
+				    </div>
+				    <div class="carousel-item">
+						<img src="assets/img/carusel_soldar.jpg" class="rounded mx-auto d-block" alt="Articulos para soldaduras.">
+						<div class="carousel-caption d-none d-md-block text-start fw-bold bg-dark bg-opacity-50 object-fit-cover rounded">
+							<p>Articulos para soldaduras.</p>
+						</div>
+				    </div>
+			  	</div>
+			  <button class="carousel-control-prev" type="button" data-bs-target="#carusel" data-bs-slide="prev">
+			    <span class="carousel-control-prev-icon bg-dark bg-opacity-50 rounded" aria-hidden="true"></span>
+			    <span class="visually-hidden">Anterior</span>
 			  </button>
-			  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-			    <span class="visually-hidden">Next</span>
+			  <button class="carousel-control-next" type="button" data-bs-target="#carusel" data-bs-slide="next">
+			    <span class="carousel-control-next-icon bg-dark bg-opacity-50 rounded" aria-hidden="true"></span>
+			    <span class="visually-hidden">Siguiente</span>
 			  </button>
 			</div>
 
 		</section>
 		<section>
 			<h1>¡Ponete en contacto con nosotros!</h1>
-			<!-- datos o form -->
+			<!-- datos -->
+			<p><b>Nuestro Local:</b> San Luis 1401</p>
+			<p><b>Por Mail:</b> todoelectroarg@gmail.com</p>
+			<p><b>Por telefono:</b> +54 379 5 12-3456</p>
+			<p><b>Nuestras redes:</b></p>
+				<ul>
+					<li><a href="https://www.facebook.com/">Facebook</a></li>
+					<li><a href="https://www.instagram.com/">Instagram</a></li>
+					<li><a href="https://www.youtube.com/">Youtube</a></li>
+					<li><a href="https://www.linkedin.com/">Linkedin</a></li>
+				</ul>
 		</section>
 
 	</main>
