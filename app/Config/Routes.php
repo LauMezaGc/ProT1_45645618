1ee58caee7c0c61e5b28b003ce5265c08671c618
lau <lautaromezagc@gmail.com>
2025-06-24 21:46:58 -0300

    controller usuario y config

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,11 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::armarPagina/inicio');
 $routes->get('/(:any)', 'Home::armarPagina/$1');
+
+$routes->get('/registro','Usuario_controller::create');
+$routes->post('/enviar-form','Usuario_controller::formValidation');
+
+$routes->get('/login', 'Login_controller');
+$routes->post('/enviarlogin','Login_controller::auth');
+$routes->get('/panel', 'Panel_controller::index',['filter' => 'auth']);
+$routes->get('/logout', 'Login_controller::logout')
